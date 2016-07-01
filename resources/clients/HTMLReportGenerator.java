63677927b8c10da9ec096321a58ccec436f0e007
Yasassri Ratnayake <ycrnet@gmail.com>
2016-07-01 11:20:38 +0530

    Adding Report Templates, Report generator client and more improvements

---
+++ resources/clients/HTMLReportGenerator.java
@@ -0,0 +1,119 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class HTMLReportGenerator {
+    public static void main(String[] args){
+        // Can parse commadling arguments for the Product type and the version
+
+        // Reading the Result File
+        String jmterLogLocation = "target/logs/result.log";
+        String htmlTemplateLocation = "";
+        String htmlReportLocation = "/home/yasassri/QA_STUFF/Patch_Automation/Tool/WSO2ESB-SmokeTester/target/reports/html/index.html";
+        String finalTableString = "";
+        String dashboardJSLocation = "/home/yasassri/QA_STUFF/Patch_Automation/Tool/WSO2ESB-SmokeTester/target/reports/html/content/js/dashboard.js";
+
+        // Counter to count success counts
+        int successCount = 0;
+        int FailureCount = 0;
+
+        String [] resultString1 = null;
+        String [] samplerString = null;
+        try {
+            resultString1 = new String(Files.readAllBytes(Paths.get(jmterLogLocation))).split("####");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Creating the table String
+        finalTableString = "<table border=\"1\" style=\"width:100%\" class=\"table table-bordered table-condensed tablesorter \"> <tr>\n" +
+                "    <th>Thread Group Name</th>\n" +
+                "    <th>Sampler Name</th>\n" +
+                "<th>Sampler Status</th>\n" +
+                "<th>Response Code</th>\n" +
+                "<th>Response Message</th>\n" +
+                "<th>Request Details - Only Shown For Errors</th>\n" +
+                "  </tr>\n";
+        for (int i = 0; i < resultString1.length; i++) {
+
+            if(resultString1[i] == null || resultString1[i].equals("")){
+                System.out.println("Result Set is Empty, Hence Continuing!!");
+                continue;
+            }
+            samplerString = resultString1[i].split(",");
+            finalTableString=finalTableString+"<tr>";
+            for(int j =0; j < samplerString.length; j++){
+                if (j==2 ){
+                    if (samplerString[j].equals("Successful")){
+                        finalTableString=finalTableString+"<td bgcolor=\"#00FF00\">" +samplerString[j]+"</td>";
+                        successCount++;
+                        continue;
+                    }else {
+                        finalTableString=finalTableString+"<td bgcolor=\"#FF0000\">" +samplerString[j]+"</td>";
+                        FailureCount++;
+                        continue;
+                    }
+                }
+                if (j==5){
+                    if (samplerString[2].equals("Successful")){
+                        finalTableString=finalTableString+"<td>See the Result Log File for Request information of Succesfull Requests</td>";
+                        continue;
+                    } else {
+                        finalTableString=finalTableString+"<td>" +samplerString[j]+"</td>";
+                        continue;
+                    }
+                }
+                finalTableString=finalTableString+"<td>" +samplerString[j]+"</td>";
+                //print(samplerRersults[j]);
+            }
+            finalTableString=finalTableString+"</tr>";
+        }
+        finalTableString=finalTableString+"</table>";
+
+        // Creaing the Summary Table
+        String summaryTable = "<table class=\"table table-bordered table-condensed tablesorter \">\n" +
+                "  <tr>\n" +
+                "    <th>Result</th>\n" +
+                "    <th>Values</th>\n" +
+                "<th>Percentage</th>\n" +
+                "  </tr>\n" +
+                "  <tr>\n" +
+                "    <td>No of Successful Requests</td>\n" +
+                "    <td>"+successCount+"</td>\n" +
+                "    <td>"+(successCount/(successCount+FailureCount))*100+"%</td>\n" +
+                "  </tr>\n" +
+                "  <tr>\n" +
+                "    <td>No of Failures</td>\n" +
+                "    <td>"+FailureCount+"</td>\n" +
+                "<td>"+(FailureCount/(successCount+FailureCount))*100+"%</td>\n" +
+                "  </tr>\n" +
+                "<tr>\n" +
+                "    <th>Total Requests </th>\n" +
+                "    <td>"+(successCount+FailureCount)+"</td>\n" +
+                "<td></td>\n" +
+                "  </tr>\n" +
+                "</table>";
+
+        try {
+            String finalReport = new String(Files.readAllBytes(Paths.get(htmlReportLocation)));
+            finalReport = finalReport.replace("${content}",finalTableString);
+            finalReport = finalReport.replace("${resultsummarytable}",summaryTable);
+
+            // Writing to the File
+            Files.write(Paths.get(htmlReportLocation), finalReport.getBytes());
+
+            // Updating the Summary Graph
+            String dashboardjs = new String(Files.readAllBytes(Paths.get(dashboardJSLocation)));
+            //var data = {"OkPercent": 50.0, "KoPercent": 50.0};
+            dashboardjs = dashboardjs.replace("${requestsummary}","\"OkPercent\": "+(successCount/(successCount+FailureCount))*100+", \"KoPercent\": "+(FailureCount/(successCount+FailureCount))*100);
+            Files.write(Paths.get(dashboardJSLocation), dashboardjs.getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Success : " +successCount + " Fails : "+FailureCount);
+
+    }
+}
--- test.sh
+++ test.sh
@@ -12,26 +12,46 @@ cleantarget=true
 echo "Jmeter Home set to $JMETER_HOME"
 
 #Creating the target directory if not exist
-if [ -d target ]
-then
-    echo "Directory already exists!"
-else
-    mkdir target
-     mkdir target/reports
-    mkdir target/reports/jtl
-    mkdir target/reports/html
-fi
+#if [ -d target ]
+#then
+#    echo "Target Directory already exists! "
+#else
+#     mkdir target
+#     mkdir target/reports
+#     mkdir target/logs
+#     mkdir target/reports/jtl
+#    mkdir target/reports/html
+#    mkdir target/javaclasses
+#fi
 
-# Cleaning OLD Results
+# Cleaning OLD Results and other content
 if [ "$cleantarget" = true ]; then
     echo "Cleaning the Target directory"
-    rm -rf $reporttarget/jtl/*
-    rm -rf $reporttarget/html/*
+    rm -rf target
 fi
 
+#Creating the target directory
+     mkdir target
+     mkdir target/reports
+     mkdir target/logs
+     mkdir target/reports/jtl
+    mkdir target/reports/html
+    mkdir target/javaclasses
+
+#Copy the report templates to target directory
+cp -R resources/template/report/* target/reports/html/
+
+echo "Starting Tests!!"
 # Running the Jmeter Scripts
 for x in $scriptlocation/*.jmx; do
   echo "Running the Script $x"
-  sh $JMETER_HOME/bin/jmeter.sh -n -t $x -l $reporttarget/jtl/test.jtl -e -o $reporttarget/html
+  sh $JMETER_HOME/bin/jmeter.sh -n -t $x -l $reporttarget/jtl/test.jtl
   #sh $JMETER_HOME/bin/jmeter.sh -n -t $x -o $reporttarget
 done
+echo "Test Runs are Over!!"
+
+# Run Report generator Java client
+# cp resources/clients/HTMLReportGenerator.java target/javaclasses
+echo "Generating the Report!!!"
+javac -d target/javaclasses resources/clients/HTMLReportGenerator.java
+java -cp target/javaclasses HTMLReportGenerator
